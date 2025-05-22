7a45a56059515368590c72d5e50ac8001d8a7920
mostofa-s-cse <mostofa.s.cse@gmail.com>
2025-05-22 10:25:44 +0600

    Remove dist directory and update .gitignore to exclude build artifacts
    - Deleted the dist directory containing compiled files to clean up the repository.
    - Updated .gitignore to include the dist directory, preventing future build artifacts from being tracked.
    - Adjusted package.json to reflect changes in the files to be included in the package.

--- dist/utils/otp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateOTP = void 0;
-const generateOTP = () => {
-    const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    return otp;
-};
-exports.generateOTP = generateOTP;
-//# sourceMappingURL=otp.js.map
